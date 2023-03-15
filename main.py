f5b9f557a6d05f898193e6de13dc48348d145f6e
Jere Kosamo <jere.kosamo@tuni.fi>
2023-03-15 14:38:30 +0200

    piste_kertoimet

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import pandas as pd
-import numpy as np
+
 import requests
-import json
-from sklearn import linear_model
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from tabulate import tabulate
 from scipy.stats import poisson
 
 
@@ -27,6 +20,10 @@ def palautusprosentti(koti, tasapeli, vieras):
 
 def lambda_kerroin(games, goals, conceded):
     kerroin = (goals / games) / (conceded / games)
+    return kerroin
+
+def lambda_pisteet(pelit, pisteet):
+    kerroin = pisteet / pelit
     return kerroin
 
 def kerroin(h_win, h_tie, h_loss, v_win, v_tie, v_loss):
@@ -106,7 +103,7 @@ if __name__ == '__main__':
     #for scoreline, probability in zip(scorelines, scoreline_probabilities):
         #print(f"{scoreline[0]}-{scoreline[1]}: {probability:.2%}")
 
-    uri = 'https://api.football-data.org/v4/competitions/PL/matches?matchday=27'
+    uri = 'https://api.football-data.org/v4/competitions/PL/matches?matchday=28'
     uri1 = 'https://api.football-data.org/v4/competitions/PL/standings?type=TOTAL'
     headers = {'X-Auth-Token': '74c7b6e85a8b49a9991a4b0a0158d38f'}
 
@@ -119,9 +116,11 @@ if __name__ == '__main__':
     games = 0
     goals = 0
     conceded = 0
+    points = 0
     opp_games = 0
     opp_goals = 0
     opp_conceded = 0
+    opp_points = 0
 
     response1 = requests.get(uri1, headers=headers)
     response = requests.get(uri, headers=headers)
@@ -140,6 +139,7 @@ if __name__ == '__main__':
                 for team_data in table_data:
                     team_name = team_data['team']['name']
                     goal_difference = team_data['goalDifference']
+                    pisteet = team_data['points']
                     maalit = team_data['goalsFor']
                     paastetyt = team_data['goalsAgainst']
                     pelit = team_data['playedGames']
@@ -154,6 +154,7 @@ if __name__ == '__main__':
                         games = pelit
                         goals = maalit
                         conceded = paastetyt
+                        points = pisteet
                     if away_team == team_name:
                         opp_wins = voitot
                         opp_draws = tasapelit
@@ -161,22 +162,29 @@ if __name__ == '__main__':
                         opp_games = pelit
                         opp_goals = maalit
                         opp_conceded = paastetyt
+                        opp_points = pisteet
 
 
 
                  # print(f"{team_name}: {goal_difference}")
         print(home_team,' vs ', away_team)
-        print(kerroin(wins, draws, losses, opp_wins, opp_draws, opp_losses))
+        #print(kerroin(wins, draws, losses, opp_wins, opp_draws, opp_losses))
 
         team1_lambda = lambda_kerroin(games, goals, conceded)
         team2_lambda = lambda_kerroin(opp_games, opp_goals, opp_conceded)
+        team1_lambda_points = lambda_pisteet(games, points)
+        team2_lambda_points = lambda_pisteet(opp_games, opp_points)
         # Calculate the probability of each team scoring a specific number of goals
         team1_goals = [poisson.pmf(i, team1_lambda) for i in range(6)]
         team2_goals = [poisson.pmf(i, team2_lambda) for i in range(6)]
+        team1_points = [poisson.pmf(i, team1_lambda_points) for i in range(6)]
+        team2_points = [poisson.pmf(i, team2_lambda_points) for i in range(6)]
 
         # Calculate the probability of each scoreline
         scorelines = [[i, j] for i in range(6) for j in range(6)]
         scoreline_probabilities = [team1_goals[i] * team2_goals[j] for i, j in scorelines]
+        scorelines_points = [[i, j] for i in range(6) for j in range(6)]
+        scoreline_probabilities_points = [team1_points[i] * team2_points[j] for i, j in scorelines_points]
 
         # Calculate the probability of each outcome
         team1_wins = sum(
@@ -185,26 +193,31 @@ if __name__ == '__main__':
             [scoreline_probabilities[i] for i in range(len(scorelines)) if scorelines[i][0] < scorelines[i][1]])
         draw = sum([scoreline_probabilities[i] for i in range(len(scorelines)) if scorelines[i][0] == scorelines[i][1]])
 
+        team1_wins_points = sum(
+            [scoreline_probabilities_points[i] for i in range(len(scorelines_points)) if scorelines_points[i][0] > scorelines_points[i][1]])
+        team2_wins_points = sum(
+            [scoreline_probabilities_points[i] for i in range(len(scorelines_points)) if scorelines_points[i][0] < scorelines_points[i][1]])
+        draw_points = sum([scoreline_probabilities_points[i] for i in range(len(scorelines_points)) if scorelines_points[i][0] == scorelines_points[i][1]])
+
         # Normalize the probabilities
         total_probability = team1_wins + team2_wins + draw
         team1_win_prob = team1_wins / total_probability
         team2_win_prob = team2_wins / total_probability
         draw_prob = draw / total_probability
 
+        total_probability_points = team1_wins_points + team2_wins_points + draw_points
+        team1_win_prob_points = team1_wins_points / total_probability_points
+        team2_win_prob_points = team2_wins_points / total_probability_points
+        draw_prob_points = draw_points / total_probability_points
+
         # Print the probabilities
-        print(f"Team 1 win probability: {team1_win_prob:.2%}")
-        print(f"Team 2 win probability: {team2_win_prob:.2%}")
-        print(f"Draw probability: {draw_prob:.2%}")
+        print(f"Team 1 win probability: {team1_win_prob:.2%}", round(1 / team1_win_prob, 2))
+        print(f"Team 2 win probability: {team2_win_prob:.2%}", round(1 / team2_win_prob, 2))
+        print(f"Draw probability: {draw_prob:.2%}", round(1 / draw_prob, 2))
 
-
-
-
-
-
-
-
-
-
+        print(f"Team 1 win probability: {team1_win_prob_points:.2%}", round(1 / team1_win_prob_points, 2))
+        print(f"Team 2 win probability: {team2_win_prob_points:.2%}", round(1 / team2_win_prob_points, 2))
+        print(f"Draw probability: {draw_prob_points:.2%}", round(1 / draw_prob_points, 2))
 
 
     #print (Ottelut)
